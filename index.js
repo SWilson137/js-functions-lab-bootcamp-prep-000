af6814d20b921efbc5ef1697c1ab1e828292f548
smiling-ping-3937 <shawn.wilson137@gmail.com>
2019-02-22 19:24:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 function happyHolidays(string){
-  return "Happy holidays!";
+  return `Happy holidays!`
 }
 
 function happyHolidaysTo(name){
-  return "Happy holidays, ${name}!";
+  return `Happy holidays, ${name}!`
 }
 
 happyHolidaysTo("you")
 
 function happyHolidayTo(holiday, name){
-  return "Happy ${holiday}, ${name}!";
+  return `Happy ${holiday}, ${name}!`
 }
 
 happyHolidaysTo("Independence Day","you")
 
 function holidayCountdown(holiday, days){
-  return "It's 20 days until Mother's Day!";
+  return `It's 20 days until Mother's Day!`
 }
